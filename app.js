1eab9c81ed185e3009574db4fc348098d1089fa0
Matthew Jensen <matthew.k.jensen@gmail.com>
2017-11-09 16:59:14 -0500

    Started OAuth

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/voting-app", {u
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    next();
-});
 
 app.use(require("express-session")({
     secret: "It's always sunny in Ferndale!",
@@ -31,9 +27,54 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.get('/polls', isLoggedIn, (req, res) => {
+    res.render('polls');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+app.post('/register', function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if (err) {
+            console.log(err);
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/polls");
+        });
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/polls",
+    failureRedirect: "/login"
+}), function(req,res){});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.port || 3000, () => {
     console.log('App Running...');
